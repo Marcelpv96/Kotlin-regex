7264624ec85bd96d9a66a804f1a485a9672f4dd8
Marcel Porta Vallés <marcelpv96@gmail.com>
2018-04-22 13:53:46 +0200

    using file as input

--- src/ex1.kt
+++ src/ex1.kt
@@ -4,22 +4,31 @@
 
 package regex;
 
-fun ex1(input : String){
+import java.io.File
+import java.io.InputStream
+
+fun ex1(input : String): Boolean {
     var expresion = Regex (
                             pattern = "Nom:\\s*[A-Z][a-z]*\\s*[A-Z][a-z]*\\s*[A-Z][a-z]*\\s*" +
                             "email:\\s*[a-zA-Z]+@+[a-zA-Z]+\\.[a-zA-Z]+\\s*" +
-                            "number:\\s*(\\p{Digit}{9}|\\+\\p{Digit}{11})"
+                            "Telf:\\s*(\\p{Digit}{9}|\\+\\p{Digit}{11})"
                             )
 
     try {
         var result = expresion.matchEntire(input)!!.value
-        println(result.equals(input))
+        return result.equals(input)
     }catch (e: KotlinNullPointerException){
-        println("false")
+        return false
     }
-
 }
 
+
 fun main(args : Array<String>){
-    ex1("Nom: Marcel Porta Valles     email: marcel@porta.com           number: +36660853339")
+    val inputStream: InputStream = File("ex1.txt").inputStream()
+    val inputString = inputStream.bufferedReader().use { it.readText() }
+
+    when {
+        ex1(inputString) -> print("Es una expresió del tipus 1.")
+        else -> print("Es una expresió incorrecta.")
+    }
 }
