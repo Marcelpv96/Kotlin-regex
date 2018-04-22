a31fdc3707a6771edfaf11baf1210133e475c5db
Marcel Porta Vallés <marcelpv96@gmail.com>
2018-04-22 13:06:32 +0200

    simple regular expresion done

--- src/ex1.kt
+++ src/ex1.kt
@@ -2,7 +2,14 @@
  * Created by Marcelpv96 on 22/4/18.
  */
 
+package regex;
 
-fun main(args : Array<String>) {
-    println("Hello, world!")
+fun ex1(input : String){
+    var expresion = Regex ("[A-Z]*")
+    var result = expresion.matchEntire(input)!!.value
+    println(result.equals(input))
+}
+
+fun main(args : Array<String>){
+    ex1("HOLA")
 }
