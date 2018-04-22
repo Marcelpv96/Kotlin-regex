1409b15fa2b10cca4810e25cdbe06f74f56def9d
Marcel Porta Vallés <marcelpv96@gmail.com>
2018-04-22 13:31:38 +0200

    ex1 done

--- src/ex1.kt
+++ src/ex1.kt
@@ -5,11 +5,21 @@
 package regex;
 
 fun ex1(input : String){
-    var expresion = Regex ("[A-Z]*")
-    var result = expresion.matchEntire(input)!!.value
-    println(result.equals(input))
+    var expresion = Regex (
+                            pattern = "Nom:\\s*[A-Z][a-z]*\\s*[A-Z][a-z]*\\s*[A-Z][a-z]*\\s*" +
+                            "email:\\s*[a-zA-Z]+@+[a-zA-Z]+\\.[a-zA-Z]+\\s*" +
+                            "number:\\s*(\\p{Digit}{9}|\\+\\p{Digit}{11})"
+                            )
+
+    try {
+        var result = expresion.matchEntire(input)!!.value
+        println(result.equals(input))
+    }catch (e: KotlinNullPointerException){
+        println("false")
+    }
+
 }
 
 fun main(args : Array<String>){
-    ex1("HOLA")
+    ex1("Nom: Marcel Porta Valles     email: marcel@porta.com           number: +36660853339")
 }
